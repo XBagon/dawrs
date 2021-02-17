0dc45ba59a449cd60d0a9404a4310b7006a30153
XBagon <xbagon@outlook.de>
2021-02-17 10:46:55 +0100

    low-pass filter works?, janky cutoff smoothing

--- src/effect/filter.rs
+++ src/effect/filter.rs
@@ -4,10 +4,11 @@ use std::collections::VecDeque;
 use rustfft::{FftPlanner, Fft};
 use rustfft::num_complex::Complex;
 use std::sync::Arc;
+use rustfft::num_traits::Zero;
 
 
 #[derive(Clone)]
-pub struct HighpassFilter {
+pub struct LowPassFilter {
     pub cutoff: f32,
     pub length: usize,
     fft_forward: Arc<dyn Fft<f32>>,
@@ -16,7 +17,7 @@ pub struct HighpassFilter {
     output_buffer: Vec<Complex<f32>>,
 }
 
-impl HighpassFilter {
+impl LowPassFilter {
     pub fn new(cutoff: f32, length: usize) -> Self {
         Self {
             cutoff,
@@ -29,19 +30,25 @@ impl HighpassFilter {
     }
 }
 
-impl Effect for HighpassFilter {
-    fn process(&mut self, _: &SampleTiming, poly_sample: PolySample) -> PolySample {
+impl Effect for LowPassFilter {
+    fn process(&mut self, sample_timing: &SampleTiming, poly_sample: PolySample) -> PolySample {
         let mut output = poly_sample!([0.0]);
         if self.buffer.len() == self.length {
             self.output_buffer.copy_from_slice(self.buffer.make_contiguous());
             self.fft_forward.process(&mut self.output_buffer);
-            //TODO: FILTER
-            //self.fft_inverse.process(&mut self.output_buffer);
+            for (i, mut z) in self.output_buffer.iter_mut().enumerate() {
+                let x = i as f32 * (sample_timing.sample_rate/self.length as f32);
+                //let y = z.re/(LENGTH as f32).sqrt();
+                //if x > self.cutoff {
+                    *z *= 1.0 - (x / self.cutoff).min(1.0);
+                //}
+            }
+            self.fft_inverse.process(&mut self.output_buffer);
             output = poly_sample!([self.output_buffer[0].re/(self.length as f32)]);
             self.buffer.pop_front().unwrap();
         }
         self.buffer.push_back(Complex { re: poly_sample[0], im: 0.0 });
-        poly_sample
+        output
     }
 }
 
@@ -50,30 +57,37 @@ mod tests {
     use crate::generator::SineGenerator;
     use crate::prelude::*;
     use crate::effect::Oscilloscope;
-    use crate::effect::filter::HighpassFilter;
+    use crate::effect::filter::LowPassFilter;
     use plotters::prelude::*;
 
     const SAMPLE_RATE: f32 = 48_000.0;
     const LENGTH: usize = 1024;
 
     struct MyPatch {
-        pub generator: SineGenerator,
-        pub filter: HighpassFilter,
+        pub generator: fn(&SampleTiming) -> PolySample,
+        pub filter: LowPassFilter,
     }
 
     impl MyPatch {
         pub fn new() -> Self {
             Self {
-                generator: SineGenerator::new(440.0),
-                filter: HighpassFilter::new(100.0, LENGTH),
+                generator: |sample_timing: &SampleTiming| {
+                    let clock = sample_timing.sample_clock();
+                    let y = (clock * 440.0 * 2.0 * std::f32::consts::PI).sin() + (clock * 247.5 * 2.0 * std::f32::consts::PI).sin() + (clock * 874.5 * 2.0 * std::f32::consts::PI).sin();
+                    poly_sample!([y * 0.1])
+                },
+                filter: LowPassFilter::new(900.0, LENGTH),
             }
         }
     }
 
     impl Patch for MyPatch {
         fn next_sample(&mut self, sample_timing: &SampleTiming) -> PolySample {
-            let poly_sample = self.generator.generate(&sample_timing);
-            self.filter.process(&sample_timing, poly_sample)
+            self.filter.cutoff = 500.0 + (sample_timing.sample_clock() * 1.0 * 4.0 * std::f32::consts::PI).sin() * 400.0;
+            let mut poly_sample = self.generator.generate(&sample_timing);
+            poly_sample = self.filter.process(&sample_timing, poly_sample);
+            poly_sample.polify(2);
+            poly_sample
         }
     }
 
@@ -81,32 +95,21 @@ mod tests {
     fn test() {
         let mut sample_timing = SampleTiming::new(48_000.0);
         let mut patch = MyPatch::new();
-        let mut oscilloscope = Oscilloscope::new((LENGTH as f32)/SAMPLE_RATE, 1.0/SAMPLE_RATE, 0, 512, 50.0);
+        let mut oscilloscope = Oscilloscope::new(0.1, 1.0/SAMPLE_RATE, 0, 512, 50.0);
 
-        for _ in 0..LENGTH*2 {
+        for _ in 0..LENGTH*8 {
             let poly_sample = patch.next_sample(&sample_timing);
             oscilloscope.process(&sample_timing, poly_sample);
             sample_timing.tick();
         }
         oscilloscope.plot("oscilloscope_output/filter_test_wave.png").unwrap();
+    }
 
-        patch.filter.process(&sample_timing, poly_sample!([0.0]));
-
-        let root = BitMapBackend::new("oscilloscope_output/filter_test_fft.png", (640, 480)).into_drawing_area();
-        root.fill(&WHITE).unwrap();
-        let mut chart = ChartBuilder::on(&root)
-            .margin(5)
-            .x_label_area_size(30)
-            .y_label_area_size(30)
-            .build_cartesian_2d(
-                0.0f32..(LENGTH as f32/2.0),
-                -10.0f32..10.0f32,
-            ).unwrap();
-        chart.configure_mesh().draw().unwrap();
-        chart
-            .draw_series(LineSeries::new(
-                patch.filter.output_buffer.iter().take(LENGTH/2).enumerate().map(|(i, z)| (i as f32 * (48_000.0/LENGTH as f32), z.re/(LENGTH as f32).sqrt())),
-                &RED,
-            )).unwrap();
+    #[test]
+    fn test_sound() {
+        let mut cpal = Cpal::new().unwrap();
+        let mut master_patch = MasterPatch::default();
+        master_patch.add_patch(MyPatch::new());
+        cpal.play_patch(&mut master_patch);
     }
 }
